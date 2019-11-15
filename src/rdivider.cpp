4da0338c4832cd852041c8ccf87316e66801f49f
Michael Schwager <mschw@fb.com>
2019-11-15 15:36:09 -0800

    Some change fns in new widget

--- src/rdivider.cpp
+++ src/rdivider.cpp
@@ -1,8 +1,95 @@
 #include "rdivider.h"
+#include <iostream>
+#include <QPainter>
+#include <QPen>
 
-RDivider::RDivider(QWidget *parent) : QWidget(parent)
-{
 
+/* assume there is margin from each of top and bottom
+ * topmost is zero, bottom-most is positive
+then:
+ 0        + margin is vMax
+ height() - margin is vMin
+ we map voltage values to pixels based on widget height
+
+
+*/
+
+double RDivider::calculateSlope(double x0, double x1) {
+    // x0 and x1 represent the min and max voltages represented
+    return (height() + 1) / (x1 - x0);
+}
+double RDivider::calculateOffset(double slope, double x0, double y0) {
+    // x0 is the minimum voltage
+    // y0 is the minimum pixel (top of box)
+    return y0 - slope * x0;
 }
 
 
+RDivider::RDivider(QWidget *parent) : QWidget(parent)
+{
+    m_vmax = 10;
+    m_vmin = 0;
+    m_margin = 10;
+    m_slope = calculateSlope(0, 10);
+    m_offset = calculateOffset(m_slope, 0, m_margin);
+}
+
+double RDivider::VMax() const {
+    return m_vmax;
+}
+void RDivider::setVMax(double x) {
+    m_vmax = x;
+    emit vmaxChanged(x);
+}
+double RDivider::VMin() const {
+    return m_vmin;
+}
+void RDivider::setVMin(double x) {
+    m_vmin = x;
+    emit vminChanged(x);
+}
+double RDivider::VTop() const {
+    return m_vtop;
+}
+void RDivider::setVTop(double x) {
+    m_vtop = x;
+    emit vtopChanged(x);
+}
+double RDivider::VBot() const {
+    return m_vbot;
+}
+void RDivider::setVBot(double x) {
+    m_vbot = x;
+    emit vbotChanged(x);
+}
+double RDivider::VMid() const {
+    return m_vmid;
+}
+void RDivider::setVMid(double x) {
+    m_vmid = x;
+    emit vmidChanged(x);
+}
+
+void RDivider::paintEvent(QPaintEvent *event) {
+    //std::cout << width() << " " << height() << std::endl;
+
+    QPainter painter(this);
+    painter.setPen(QPen(Qt::blue));
+    painter.drawRect(0,0, width()-1, height()-1);
+}
+
+void RDivider::mouseMoveEvent(QMouseEvent *event) {
+    std::cout << "Move" << std::endl;
+}
+
+void RDivider::mousePressEvent(QMouseEvent *event) {
+    std::cout << "press" << std::endl;
+}
+
+void RDivider::resizeEvent(QResizeEvent *event) {
+    m_slope = calculateSlope(m_vmin, m_vmax);
+    m_offset = calculateOffset(m_slope, m_vmin, m_margin);
+//    std::cout << "resize" << std::endl;
+//    std::cout << width() << " " << height() << std::endl;
+    std::cout << m_slope << " " << m_offset << std::endl;
+}
--- src/rdivider.h
+++ src/rdivider.h
@@ -3,14 +3,49 @@
 
 #include <QWidget>
 
+
+
+
 class RDivider : public QWidget
 {
     Q_OBJECT
+    Q_PROPERTY(double VMax READ VMax WRITE setVMax NOTIFY vmaxChanged)
+    Q_PROPERTY(double VMin READ VMin WRITE setVMin NOTIFY vminChanged)
+    Q_PROPERTY(double VTop READ VTop WRITE setVTop NOTIFY vtopChanged)
+    Q_PROPERTY(double VBot READ VBot WRITE setVBot NOTIFY vbotChanged)
+    Q_PROPERTY(double VMid READ VMid WRITE setVMid NOTIFY vmidChanged)
+
+    double m_vmax, m_vmin, m_vtop, m_vbot, m_vmid;
+    double m_slope, m_offset;
+    double m_margin;
+
+    double calculateSlope(double x0, double x1);
+    double calculateOffset(double slope, double x0, double y0);
+
 public:
     explicit RDivider(QWidget *parent = nullptr);
-    void paintEvent(QPaintEvent *event) override;
-
+    double VMax() const;
+    void setVMax(double);
+    double VMin() const;
+    void setVMin(double);
+    double VTop() const;
+    void setVTop(double);
+    double VBot() const;
+    void setVBot(double);
+    double VMid() const;
+    void setVMid(double);
+protected:
+    void paintEvent(QPaintEvent *) override;
+    void mouseMoveEvent(QMouseEvent *) override;
+    void mousePressEvent(QMouseEvent *) override;
+    void resizeEvent(QResizeEvent *) override;
+    
 signals:
+    void vmaxChanged(double);
+    void vminChanged(double);
+    void vtopChanged(double);
+    void vbotChanged(double);
+    void vmidChanged(double);
 
 public slots:
 };
